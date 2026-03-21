06f96ae05c33ef2be923e72bbacb07984db4fad3
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-03-21 15:40:01 -0500

    chore: ruff fixes

--- cyberdrop_dl/director.py
+++ cyberdrop_dl/director.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import asyncio
 import contextlib
 import logging
-import sys
 from datetime import datetime
 from enum import IntEnum
 from functools import wraps
