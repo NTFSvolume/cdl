e75df7e5b2cd09686c6eb33dd894ff078589a690
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-03 09:25:31 -0500

    refactor: simplify

--- cyberdrop_dl/ui/prompts/basic_prompts.py
+++ cyberdrop_dl/ui/prompts/basic_prompts.py
@@ -1,35 +1,22 @@
-# type: ignore[reportPrivateImportUsage]
 import sys
 from pathlib import Path
 
-from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
+from InquirerPy.prompts.filepath import FilePathPrompt
 from InquirerPy.validator import PathValidator
 
-from cyberdrop_dl.ui.prompts.defaults import DEFAULT_OPTIONS
 
-
-def ask_choice(choices: list[Choice], *, message: str = "What would you like to do:", **kwargs):
-    options = DEFAULT_OPTIONS | kwargs
-    return inquirer.select(message=message, choices=choices, **options).execute()
-
-
-def ask_path(message: str = "Select path", *, validator_options: dict | None = None, **kwargs) -> Path:
-    options = DEFAULT_OPTIONS | {"default": str(Path.home())} | kwargs
+def ask_dir_path(message: str = "Select dir path") -> Path:
     return Path(
-        inquirer.filepath(message=message, validate=PathValidator(**(validator_options or {})), **options).execute()
+        FilePathPrompt(
+            message=message,
+            validate=PathValidator(is_dir=True, message="Input is not a directory"),
+            long_instruction="ARROW KEYS: Navigate | ENTER: Select",
+            default=str(Path.home()),
+        ).execute(),
     )
 
 
-def ask_dir_path(message: str = "Select dir path", **kwargs) -> Path:
-    options = DEFAULT_OPTIONS | kwargs
-    validator_options = {"is_dir": True, "message": "Input is not a directory"}
-    return ask_path(message, validator_options=validator_options, **options)
-
-
-def enter_to_continue(message: str = "Press <ENTER> to continue", **kwargs):
+def enter_to_continue() -> None:
     if "pytest" in sys.modules:
         return
-    options = DEFAULT_OPTIONS | {"long_instruction": "ENTER: continue"} | kwargs
-    msg = f"\n{message}"
-    return inquirer.confirm(message=msg, qmark="", **options).execute()
+    _ = input("Press <ENTER> to continue")
--- cyberdrop_dl/ui/prompts/user_prompts.py
+++ cyberdrop_dl/ui/prompts/user_prompts.py
@@ -1,38 +1,43 @@
-# type: ignore[reportPrivateImportUsage]
 from __future__ import annotations
 
-from collections.abc import Mapping
 from typing import TYPE_CHECKING
 
 from InquirerPy.base.control import Choice
+from InquirerPy.prompts.list import ListPrompt
 from InquirerPy.separator import Separator
 from rich.console import Console
 
 from cyberdrop_dl import __version__
-from cyberdrop_dl.ui.prompts import basic_prompts
+from cyberdrop_dl.ui.prompts.basic_prompts import enter_to_continue
 from cyberdrop_dl.ui.prompts.defaults import EXIT_CHOICE
 from cyberdrop_dl.utils.utilities import clear_term
 
 if TYPE_CHECKING:
+    from collections.abc import Generator, Iterable
+
     from cyberdrop_dl.managers.manager import Manager
 
-from cyberdrop_dl.ui.prompts.defaults import DONE_CHOICE
 
 console = Console()
 
 
 def main_prompt(manager: Manager) -> int:
-    """Main prompt for the program."""
     prompt_header(manager)
-    OPTIONS = {
-        "group_1": ["Download", "Retry failed downloads", "Create file hashes", "Sort files in download folder"],
-        "group_2": ["Edit URLs.txt"],
-        "group_3": ["View changelog"],
-    }
+    enter_to_continue()
+    choices = create_choices(
+        [
+            [
+                "Download",
+                "Retry failed downloads",
+                "Create file hashes",
+                "Sort files in download folder",
+            ],
+            ["Edit URLs.txt"],
+            ["View changelog"],
+        ]
+    )
 
-    choices = create_choices(OPTIONS, append_last=EXIT_CHOICE)
-
-    return basic_prompts.ask_choice(choices)
+    return ask_choice(list(choices))
 
 
 def prompt_header(manager: Manager, title: str | None = None) -> None:
@@ -42,27 +47,19 @@ def prompt_header(manager: Manager, title: str | None = None) -> None:
     console.print(f"[bold]Current config:[/bold] [blue]{manager.config_manager.loaded_config}[/blue]")
 
 
-def create_choices(
-    options_groups: list[list[str]] | Mapping[str, list[list[str]]],
-    append_last: Choice = DONE_CHOICE,
-    *,
-    disabled_choices: list[str] | None = None,
-):
-    if isinstance(options_groups, Mapping):
-        options_groups = list(options_groups.values())
-    disabled_choices = disabled_choices or []
-    options = [option for group in options_groups for option in group]
-    choices = []
-    for index, option in enumerate(options, 1):
-        enabled = option not in disabled_choices
-        choices.append(Choice(index, option, enabled))
-    choices.append(append_last)
-
-    separator_indexes = []
+def create_choices(options_groups: Iterable[Iterable[str]]) -> Generator[Choice | Separator]:
     for group in options_groups:
-        separator_indexes.append(len(group) + (separator_indexes[-1] if separator_indexes else 0))
+        for index, option in enumerate(group, 1):
+            yield Choice(index, option, enabled=True)
 
-    for count, index in enumerate(separator_indexes):
-        choices.insert(index + count, Separator())
+        yield Separator()
 
-    return choices
+    yield EXIT_CHOICE
+
+
+def ask_choice(choices: list[Choice | Separator]) -> int:
+    return ListPrompt(
+        message="What would you like to do:",
+        choices=choices,
+        long_instruction="ARROW KEYS: Navigate | ENTER: Select",
+    ).execute()
