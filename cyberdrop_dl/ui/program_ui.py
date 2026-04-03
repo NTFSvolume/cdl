c9ce948353bef459091bf48f5161eda2fa087e3b
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-03 09:58:34 -0500

    refactor: simplify

--- cyberdrop_dl/ui/program_ui.py
+++ cyberdrop_dl/ui/program_ui.py
@@ -1,31 +1,38 @@
 from __future__ import annotations
 
 import asyncio
-import functools
 import sys
+from pathlib import Path
 from typing import TYPE_CHECKING
 
-from requests import request
+import aiohttp
+from InquirerPy.base.control import Choice
+from InquirerPy.prompts.filepath import FilePathPrompt
+from InquirerPy.prompts.list import ListPrompt
+from InquirerPy.separator import Separator
+from InquirerPy.validator import PathValidator
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.text import Text
 
+from cyberdrop_dl import __version__
 from cyberdrop_dl.clients.hash_client import hash_directory_scanner
-from cyberdrop_dl.ui.prompts import DONE_CHOICE, EXIT_CHOICE, ask_dir_path, enter_to_continue, main_prompt
 from cyberdrop_dl.utils import text_editor
 from cyberdrop_dl.utils.sorting import Sorter
 from cyberdrop_dl.utils.utilities import clear_term
 
 if TYPE_CHECKING:
-    from pathlib import Path
-
-    from InquirerPy.base.control import Choice
+    from collections.abc import Generator, Iterable
 
     from cyberdrop_dl.managers.manager import Manager
 
 
 console = Console()
 ERROR_PREFIX = Text("ERROR: ", style="bold red")
+_changelog: str = ""
+_CHANGELOG_URL = "https://raw.githubusercontent.com/NTFSvolume/cdl/refs/heads/main/CHANGELOG.md"
+_EXIT_CHOICE = Choice("Exit")
+_DONE_CHOICE = Choice("Done")
 
 
 class ProgramUI:
@@ -34,14 +41,6 @@ class ProgramUI:
         if run:
             self.run()
 
-    @staticmethod
-    def print_error(msg: str, critical: bool = False) -> None:
-        text = ERROR_PREFIX + msg
-        console.print(text, style="bold red" if critical else None)
-        if critical:
-            sys.exit(1)
-        enter_to_continue()
-
     def run(self) -> None:
         done = False
         while not done:
@@ -49,12 +48,12 @@ class ProgramUI:
 
     def _run(self) -> Choice | bool | None:
         clear_term()
-        answer = main_prompt(self.manager)
+        answer = _main_prompt(self.manager)
 
-        if answer == EXIT_CHOICE.value:
+        if answer == _EXIT_CHOICE.value:
             sys.exit(0)
-        if answer == DONE_CHOICE.value:
-            return DONE_CHOICE
+        if answer == _DONE_CHOICE.value:
+            return _DONE_CHOICE
 
         return {
             1: self._download,
@@ -66,22 +65,21 @@ class ProgramUI:
         }[answer]()
 
     def _download(self) -> bool:
-        """Starts download process."""
         return True
 
     def _retry_failed_download(self) -> bool:
-        """Sets retry failed and starts download process."""
         self.manager.parsed_args.cli_only_args.retry_failed = True
         return True
 
     def _scan_and_create_hashes(self) -> None:
-        """Scans a folder and creates hashes for all of its files."""
-        path = ask_dir_path("Select the directory to scan", default=self.manager.config.files.download_folder)
+        path = ask_dir_path(
+            "Select the directory to scan",
+            default=self.manager.config.files.download_folder,
+        )
         asyncio.run(hash_directory_scanner(self.manager, path))
         enter_to_continue()
 
     def _sort_files(self) -> None:
-        """Sort files in download folder"""
         sorter = Sorter.from_manager(self.manager)
         console.print(
             f"You are about to sort files from '{sorter.input_dir}' to '{sorter.output_dir}'", style="bold red"
@@ -92,39 +90,93 @@ class ProgramUI:
             enter_to_continue()
 
     def _view_changelog(self) -> None:
+        global _changelog
+
         clear_term()
-        try:
-            changelog_content = _get_changelog()
-        except Exception:
-            self.print_error("UNABLE TO GET CHANGELOG INFORMATION")
-            return None
+
+        if not _changelog:
+            try:
+                _changelog = asyncio.run(_get_changelog())
+            except Exception:
+                console.print(ERROR_PREFIX, "UNABLE TO GET CHANGELOG INFORMATION")
+                enter_to_continue()
+                return None
 
         with console.pager(links=True):
-            console.print(Markdown(changelog_content, justify="left"))
+            console.print(Markdown(_changelog, justify="left"))
 
     def _edit_urls(self) -> None:
-        self._open_in_text_editor(self.manager.config.files.input_file, reload_config=False)
-
-    def _open_in_text_editor(self, file_path: Path, *, reload_config: bool = True):
         try:
-            text_editor.open(file_path)
+            text_editor.open(self.manager.config.files.input_file)
         except ValueError as e:
-            self.print_error(str(e))
-            return
-        if reload_config:
-            console.print("Revalidating config, please wait..")
-            self.manager.config_manager.reload_config()
+            console.print(ERROR_PREFIX, str(e))
+            enter_to_continue()
 
 
-@functools.cache
-def _get_changelog() -> str:
-    """Get latest changelog file from github. Returns its content."""
-
-    url = "https://raw.githubusercontent.com/NTFSvolume/cdl/refs/heads/main/CHANGELOG.md"
-    with request("GET", url, timeout=15) as response:
-        response.raise_for_status()
-        content = response.text
+async def _get_changelog() -> str:
+    async with aiohttp.request("GET", _CHANGELOG_URL, raise_for_status=True) as response:
+        content = await response.text()
 
     lines = content.splitlines()
     # remove keep_a_changelog disclaimer
     return "\n".join(lines[:21] + lines[25:])
+
+
+def _main_prompt(manager: Manager) -> int:
+    _prompt_header(manager)
+    enter_to_continue()
+    choices = _create_choices(
+        [
+            [
+                "Download",
+                "Retry failed downloads",
+                "Create file hashes",
+                "Sort files in download folder",
+            ],
+            ["Edit URLs.txt"],
+            ["View changelog"],
+        ]
+    )
+
+    return _ask_choices(list(choices))
+
+
+def _prompt_header(manager: Manager) -> None:
+    clear_term()
+    console.print(f"[bold]Cyberdrop Downloader ([blue]V{__version__!s}[/blue])[/bold]")
+    console.print(f"[bold]Current config:[/bold] [blue]{manager.config_manager.loaded_config}[/blue]")
+
+
+def _create_choices(options_groups: Iterable[Iterable[str]]) -> Generator[Choice | Separator]:
+    for group in options_groups:
+        for index, option in enumerate(group, 1):
+            yield Choice(index, option, enabled=True)
+
+        yield Separator()
+
+    yield _EXIT_CHOICE
+
+
+def _ask_choices(choices: list[Choice | Separator]) -> int:
+    return ListPrompt(
+        message="What would you like to do:",
+        choices=choices,
+        long_instruction="ARROW KEYS: Navigate | ENTER: Select",
+    ).execute()
+
+
+def ask_dir_path(message: str = "Select dir path", default: Path = Path.home()) -> Path:  # noqa: B008
+    return Path(
+        FilePathPrompt(
+            message=message,
+            validate=PathValidator(is_dir=True, message="Input is not a directory"),
+            long_instruction="ARROW KEYS: Navigate | ENTER: Select",
+            default=str(default),
+        ).execute(),
+    )
+
+
+def enter_to_continue() -> None:
+    if "pytest" in sys.modules:
+        return
+    _ = input("Press <ENTER> to continue")
