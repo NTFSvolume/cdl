4ced7724c68585b9ed4b75d3f49864347fea6e45
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-05 11:58:31 -0500

    refactor: update rich logger classes

--- cyberdrop_dl/utils/logger.py
+++ cyberdrop_dl/utils/logger.py
@@ -1,28 +1,35 @@
 from __future__ import annotations
 
+import contextlib
+import itertools
 import json
 import logging
 import queue
+from contextvars import ContextVar
 from logging.handlers import QueueHandler, QueueListener
 from pathlib import Path
-from typing import IO, TYPE_CHECKING, ParamSpec
+from typing import TYPE_CHECKING, ParamSpec, cast
 
 from rich._log_render import LogRender
 from rich.console import Console, Group
-from rich.containers import Lines, Renderables
 from rich.logging import RichHandler
-from rich.measure import Measurement
 from rich.padding import Padding
 from rich.text import Text, TextType
+from typing_extensions import override
 
-from cyberdrop_dl import constants
+if TYPE_CHECKING:
+    import threading
+    from collections.abc import Generator
 
 logger = logging.getLogger("cyberdrop_dl")
 _DEFAULT_CONSOLE = Console()
 
-_USER_NAME = Path.home().resolve().name
+_USER_NAME = Path.home().name
 _NEW_ISSUE_URL = "https://github.com/NTFSvolume/cdl/issues/new/choose"
 _DEFAULT_CONSOLE_WIDTH = 240
+_LOCK: threading.RLock = cast("threading.RLock", logging._lock)  # pyright: ignore[ reportAttributeAccessIssue]
+
+_capture_logs: ContextVar[bool] = ContextVar("_capture_logs", default=False)
 
 
 if TYPE_CHECKING:
@@ -40,18 +47,28 @@ if TYPE_CHECKING:
 class RedactedConsole(Console):
     """Custom console to remove username from logs"""
 
+    @override
     def _render_buffer(self, buffer) -> str:
-        output: str = super()._render_buffer(buffer)
-        return _redact_message(output)
+        return self._redact_message(super()._render_buffer(buffer))
+
+    @classmethod
+    def _redact_message(cls, message: object) -> str:
+        redacted = str(message)
+        for sep in ("\\", "\\\\", "/"):
+            as_tail = sep + _USER_NAME
+            as_part = _USER_NAME + sep
+            redacted = redacted.replace(as_tail, f"{sep}[REDACTED]").replace(as_part, f"[REDACTED]{sep}")
+        return redacted
 
 
 class JsonLogRecord(logging.LogRecord):
-    def getMessage(self) -> str:  # noqa: N802
-        """`dicts` will be logged as json, lazily"""
+    """`dicts` will be logged as json, lazily"""
 
+    @override
+    def getMessage(self) -> str:
         msg = str(self._proccess_msg(self.msg))
         if self.args:
-            args = map(self._proccess_msg, self.args)
+            args = tuple(map(self._proccess_msg, self.args))
             try:
                 return msg.format(*args)
             except Exception:
@@ -62,6 +79,8 @@ class JsonLogRecord(logging.LogRecord):
     @staticmethod
     def _proccess_msg(msg: object) -> object:
         # TODO: Use our custom decoder to support more types
+        if callable(msg):
+            msg = msg()
         if isinstance(msg, dict):
             return json.dumps(msg, indent=2, ensure_ascii=False, default=str)
         return msg
@@ -71,35 +90,57 @@ logging.setLogRecordFactory(JsonLogRecord)
 
 
 class LogHandler(RichHandler):
-    """Rich Handler with default settings, automatic console creation and custom log render to remove padding in files."""
+    """Rich Handler with default settings, custom log render to remove padding in files and `color` extra"""
 
-    def __init__(
-        self, level: int = 10, file: IO[str] | None = None, width: int | None = None, debug: bool = False
-    ) -> None:
-        is_file: bool = file is not None
-        redacted: bool = is_file and not debug
-        console_cls = RedactedConsole if redacted else Console
-        if file is None and width is None:
-            console = _DEFAULT_CONSOLE
-        else:
-            console = console_cls(file=file, width=width)
-
+    def __init__(self, level: int = logging.DEBUG, console: Console | None = None) -> None:
+        self.is_file: bool = bool(console)
+        self._buffer: list[Text] = []
         super().__init__(
             level,
             console,
-            show_time=False,
-            show_path=False,
-            tracebacks_show_locals=debug,
-            locals_max_string=constants.DEFAULT_CONSOLE_WIDTH,
+            show_time=self.is_file,
+            rich_tracebacks=True,
+            tracebacks_show_locals=True,
+            locals_max_string=_DEFAULT_CONSOLE_WIDTH,
             tracebacks_extra_lines=2,
             locals_max_length=20,
         )
-        if is_file:
-            self._log_render = NoPaddingLogRender(
-                show_path=False,
-                show_level=True,
-                time_format=lambda dt: Text(f"[{dt.isoformat(sep=' ', timespec='milliseconds')}]", style="log.time"),
-            )
+        if self.is_file:
+            self._log_render = NoPaddingLogRender(show_level=True)
+
+    @override
+    def render_message(self, record: logging.LogRecord, message: str) -> ConsoleRenderable:
+        """This is the same as the base class, just added the `color` parsing from the extras"""
+        use_markup = bool(getattr(record, "markup", self.markup))
+        color = getattr(record, "color", "")
+        message_text = Text.from_markup(message, style=color) if use_markup else Text(message, style=color)
+
+        highlighter = getattr(record, "highlighter", self.highlighter)
+        if highlighter:
+            message_text = highlighter(message_text)
+
+        if self.keywords is None:
+            self.keywords = self.KEYWORDS
+
+        if self.keywords:
+            _ = message_text.highlight_words(self.keywords, "logging.keyword")
+
+        if self.is_file and _capture_logs.get():
+            self._buffer.append(message_text)
+
+        return message_text
+
+    def export_text(self) -> Text:
+        assert self.lock is not None
+        with self.lock:
+            lines = self._buffer[:]
+            self._buffer.clear()
+
+        eof = Text("\n")
+        text = Text()
+        for line in itertools.chain.from_iterable((line, eof) for line in lines):
+            _ = text.append_text(line)
+        return text
 
 
 class BareQueueHandler(QueueHandler):
@@ -111,7 +152,7 @@ class BareQueueHandler(QueueHandler):
     This made tracebacks render improperly because when the rich handler picks the log record from the queue, it has no traceback.
     The original traceback was being formatted as normal text and included as part of the message.
 
-    Having not pickleable objects is only an issue in multi-processing operations (multiprocessing.Queue)
+    We never log from other processes so we do not need that safety check
     """
 
     def prepare(self, record: logging.LogRecord) -> logging.LogRecord:
@@ -138,11 +179,31 @@ class QueuedLogger:
         self.log_handler.close()
 
 
+@contextlib.contextmanager
+def _lazy_logger(log_handler: LogHandler) -> Generator[BareQueueHandler]:
+    """Context-manager to process logs from this handler in another thread.
+
+    It starts a QueueListener and yields the QueueHandler."""
+    q: queue.Queue[logging.LogRecord] = queue.Queue()
+    queue_handler: BareQueueHandler = BareQueueHandler(q)
+    listener: QueueListener = QueueListener(q, log_handler, respect_handler_level=True)
+    listener.start()
+    try:
+        yield queue_handler
+    finally:
+        try:
+            queue_handler.close()
+        finally:
+            listener.stop()
+            for handl in listener.handlers[:]:
+                handl.close()
+
+
 class NoPaddingLogRender(LogRender):
-    cdl_padding: int = 0
+    _cdl_padding: int = 0
     EXCLUDE_PATH_LOGGING_FROM: tuple[str, ...] = "logger.py", "base.py", "session.py", "cache_control.py"
 
-    def __call__(  # type: ignore[reportIncompatibleMethodOverride]
+    def __call__(  # type: ignore[reportIncompatibleMethodOverride]  # pyright: ignore[reportIncompatibleMethodOverride]
         self,
         console: Console,
         renderables: Iterable[ConsoleRenderable],
@@ -169,67 +230,52 @@ class NoPaddingLogRender(LogRender):
                 output.append(log_time_display)
                 output.pad_right(1)
                 self._last_time = log_time_display
+
         if self.show_level:
             output.append(level)
             output.pad_right(1)
 
-        if not self.cdl_padding:
-            self.cdl_padding = _get_renderable_length(output)
+        if not self._cdl_padding:
+            self._cdl_padding = console.measure(output).maximum
 
         if self.show_path and path and not any(path.startswith(p) for p in self.EXCLUDE_PATH_LOGGING_FROM):
             path_text = Text(style="log.path")
-            path_text.append(path, style=f"link file://{link_path}" if link_path else "")
+            _ = path_text.append(path, style=f"link file://{link_path}" if link_path else "")
             if line_no:
-                path_text.append(":")
-                path_text.append(
+                _ = path_text.append(":")
+                _ = path_text.append(
                     f"{line_no}",
                     style=f"link file://{link_path}#{line_no}" if link_path else "",
                 )
-            output.append(path_text)
+            _ = output.append_text(path_text)
             output.pad_right(1)
 
         padded_lines: list[ConsoleRenderable] = []
 
-        for renderable in Renderables(renderables):  # type: ignore
+        for renderable in renderables:
             if isinstance(renderable, Text):
-                renderable = _indent_text(renderable, console, self.cdl_padding)
+                renderable = _indent_text(renderable, console, self._cdl_padding)
                 renderable.stylize("log.message")
-                output.append(renderable)
+                _ = output.append(renderable)
                 continue
-            padded_lines.append(Padding(renderable, (0, 0, 0, self.cdl_padding), expand=False))
+
+            padded_lines.append(Padding(renderable, (0, 0, 0, self._cdl_padding), expand=False))
 
         return Group(output, *padded_lines)
 
 
-def _get_renderable_length(renderable) -> int:
-    measurement = Measurement.get(_DEFAULT_CONSOLE, _DEFAULT_CONSOLE.options, renderable)
-    return measurement.maximum
-
-
-def _indent_text(text: Text, console: Console, indent: int = 30) -> Text:
+def _indent_text(text: Text, console: Console, indent: int) -> Text:
     """Indents each line of a Text object except the first one."""
-    indent_str = Text("\n" + (" " * indent))
-    new_text = Text()
-    new_width = console.width - indent
-    lines: Lines = text.wrap(console, width=new_width)
-    first_line = lines[0]
-    other_lines = lines[1:]
-    for line in other_lines:
-        line.rstrip()
-        new_text.append(indent_str + line)
+    padding = Text("\n" + (" " * indent))
+    first_line, *rest = text.wrap(console, width=console.width - indent)
     first_line.rstrip()
-    return first_line.append(new_text)
+    text = first_line
+    for line in rest:
+        line.rstrip()
+        _ = text.append_text(padding.append_text(line))
+
+    return text
 
 
 def log_spacer(char: str = "-") -> None:
     logger.info(char * (_DEFAULT_CONSOLE_WIDTH // 2), stacklevel=2)
-
-
-def _redact_message(message: Exception | Text | str) -> str:
-    redacted = str(message)
-    separators = ["\\", "\\\\", "/"]
-    for sep in separators:
-        as_tail = sep + _USER_NAME
-        as_part = _USER_NAME + sep
-        redacted = redacted.replace(as_tail, f"{sep}[REDACTED]").replace(as_part, f"[REDACTED]{sep}")
-    return redacted
