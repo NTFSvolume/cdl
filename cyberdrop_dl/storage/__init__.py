1c6b832820658994a7a8ce34100af3f10ceb4a07
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-08 00:45:43 -0500

    fix: fix it

--- cyberdrop_dl/storage/__init__.py
+++ cyberdrop_dl/storage/__init__.py
@@ -20,12 +20,12 @@ if TYPE_CHECKING:
 _required_free_space: ContextVar[int] = ContextVar("_required_free_space")
 
 try:
-    from ._psutil import has_sufficient_space
+    from ._psutil import has_sufficient_space as _has_sufficient_space
     from ._psutil import start_loop as _psutil_loop
 except ImportError:
     _psutil_loop = None
 
-    async def has_sufficient_space(folder: Path, /, required_free_space: int) -> bool:
+    async def _has_sufficient_space(folder: Path, /, required_free_space: int) -> bool:
         usage = await asyncio.to_thread(shutil.disk_usage, folder)
         return usage.free > required_free_space
 
@@ -36,35 +36,34 @@ def create_free_space_checker(media_item: MediaItem, *, frecuency: int = 5) -> C
     async def checker() -> None:
         nonlocal current_chunk
         if current_chunk % frecuency == 0:
-            await check(media_item)
+            if not await has_sufficient_space(media_item.download_folder):
+                raise InsufficientFreeSpaceError(media_item)
+
         current_chunk += 1
 
     return checker
 
 
-async def check(media_item: MediaItem) -> None:
-    """Checks if there is enough free space to download this item."""
-
-    if not await has_sufficient_space(media_item.download_folder, _required_free_space.get()):
-        raise InsufficientFreeSpaceError(media_item)
+async def has_sufficient_space(folder: Path, /) -> bool:
+    return await _has_sufficient_space(folder, _required_free_space.get())
 
 
 @contextlib.asynccontextmanager
 async def monitor(required_free_space: int) -> AsyncGenerator[None]:
+    token = _required_free_space.set(required_free_space)
     if _psutil_loop is None:
         logger.warning("psutil is not available on this system. Falling back to eager checks for free space")
-        yield
-        return
-
-    loop = asyncio.create_task(_psutil_loop(), name="storage monitor")
-    token = _required_free_space.set(required_free_space)
-    await asyncio.sleep(0)
+        loop = None
+    else:
+        loop = asyncio.create_task(_psutil_loop(), name="storage monitor")
+        await asyncio.sleep(0)
     try:
         yield
     finally:
         _required_free_space.reset(token)
-        try:
-            _ = loop.cancel("On monitor exit")
-            await loop
-        except asyncio.CancelledError:
-            pass
+        if loop is not None:
+            try:
+                _ = loop.cancel("On monitor exit")
+                await loop
+            except asyncio.CancelledError:
+                pass
