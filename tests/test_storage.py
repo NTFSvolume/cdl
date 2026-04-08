9d0331548588debc531a6eb56b4a146f7658f300
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-08 06:59:39 +0000

    refactor: wording
    Co-authored-by: NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -91,14 +91,14 @@ async def test_no_psutil_check_does_not_raise_exception(tmp_path: Path) -> None:
             assert await storage.has_sufficient_space(tmp_path)
 
 
-def test_no_psutil_returns_size_of_closes_parent_on_file_that_does_not_exists(tmp_path: Path) -> None:
+def test_no_psutil_returns_size_of_closest_parent_on_file_that_does_not_exists(tmp_path: Path) -> None:
     folder = tmp_path / "folder_abc/that/does/not/exists"
     result = storage._disk_usage(folder)
     assert result > 0
     assert result == storage._disk_usage(tmp_path)
 
 
-async def test_psutil_returns_size_of_closes_parent_on_file_that_does_not_exists(tmp_path: Path) -> None:
+async def test_psutil_returns_size_of_closest_parent_on_file_that_does_not_exists(tmp_path: Path) -> None:
     folder = tmp_path / "folder_abc/that/does/not/exists"
     result = await _psutil.get_free_space(folder)
     assert result > 0
