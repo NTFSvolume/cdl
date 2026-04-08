d883e427d7d06f6ea8df9a0cfb56e96228077843
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-08 00:45:23 -0500

    tests: add regresion tests: when psutil is not available

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -7,56 +7,56 @@ from unittest import mock
 
 import pytest
 
-from cyberdrop_dl import aio
-from cyberdrop_dl.storage import _psutil as storage
+from cyberdrop_dl import aio, storage
+from cyberdrop_dl.storage import _psutil
 
 
 def create_partition(path: str):
-    return storage.DiskPartition(Path(path), Path(path), "", "")
+    return _psutil.DiskPartition(Path(path), Path(path), "", "")
 
 
 def find_partition(path: str):
-    return storage._find_partition(Path(path))
+    return _psutil._find_partition(Path(path))
 
 
 async def test_unsupported_fs_should_not_return_zero() -> None:
     cwd = await aio.resolve(Path())
-    free_space = await storage._get_free_space(cwd)
+    free_space = await _psutil._get_free_space(cwd)
     assert free_space > 0
     with mock.patch("psutil.disk_usage", side_effect=OSError(None, "operation not supported")):
-        free_space = await storage._get_free_space(cwd)
+        free_space = await _psutil._get_free_space(cwd)
         assert free_space == -1
 
     with mock.patch("psutil.disk_usage", side_effect=OSError(None, "another error")):
         with pytest.raises(OSError):
-            _ = await storage._get_free_space(cwd)
+            _ = await _psutil._get_free_space(cwd)
 
 
 async def test_fuse_filesystem_should_not_return_zero() -> None:
     cwd = await aio.resolve(Path())
-    partition = storage._find_partition(cwd)
+    partition = _psutil._find_partition(cwd)
     assert partition
-    assert not storage._is_fuse_fs(cwd)
-    storage._PARTITIONS = [dataclasses.replace(partition, fstype="fuse")]  # pyright: ignore[reportPrivateUsage]
-    assert storage._is_fuse_fs(cwd)
+    assert not _psutil._is_fuse_fs(cwd)
+    _psutil._PARTITIONS = [dataclasses.replace(partition, fstype="fuse")]  # pyright: ignore[reportPrivateUsage]
+    assert _psutil._is_fuse_fs(cwd)
 
-    free_space = await storage._get_free_space(cwd)
+    free_space = await _psutil._get_free_space(cwd)
     assert free_space > 0
 
     class NullUsage:
         free = 0
 
     with mock.patch("psutil.disk_usage", return_value=NullUsage()):
-        free_space = await storage._get_free_space(cwd)
+        free_space = await _psutil._get_free_space(cwd)
         assert free_space == -1
 
 
 def test_storage_only_work_with_abs_paths() -> None:
     cwd = Path()
     with pytest.raises(ValueError):
-        _ = storage._find_partition(cwd)
+        _ = _psutil._find_partition(cwd)
 
-    assert storage._find_partition(cwd.resolve())
+    assert _psutil._find_partition(cwd.resolve())
 
 
 @pytest.mark.skipif(os.name == "nt", reason="Test paths are only posix")
@@ -66,7 +66,7 @@ async def test_find_partition_finds_the_correct_partition() -> None:
         create_partition(path) for path in ("/", "/home", "/mnt/USB", "/home/external_SSD")
     ]
 
-    storage._PARTITIONS = partitions  # pyright: ignore[reportPrivateUsage]
+    _psutil._PARTITIONS = partitions  # pyright: ignore[reportPrivateUsage]
 
     assert find_partition("/swap_file") is root
     assert find_partition("/home/user/.bash_rc") is home
@@ -79,9 +79,15 @@ async def test_find_partition_finds_the_correct_partition() -> None:
 async def test_find_partition_finds_the_correct_partition_windows() -> None:
     c_drive, d_drive = partitions = [create_partition(path) for path in ("C:/", "D:/")]
 
-    storage._PARTITIONS = partitions  # pyright: ignore[reportPrivateUsage]
+    _psutil._PARTITIONS = partitions  # pyright: ignore[reportPrivateUsage]
 
     assert find_partition("C:/pagefile") is c_drive
     assert find_partition("C:/") is c_drive
     assert find_partition("D:/music/song.mp3") is d_drive
     assert find_partition("Z:/") is None
+
+
+async def test_no_psutil_still_works(tmp_path: Path) -> None:
+    with mock.patch.object(storage, "_psutil_loop", None):
+        async with storage.monitor(100):
+            assert await storage.has_sufficient_space(tmp_path)
