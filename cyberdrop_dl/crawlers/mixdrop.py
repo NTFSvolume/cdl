11da9940fd3bca4270a8728628677d20f35d0bf7
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-10 21:45:04 -0500

    refactor: simplify

--- cyberdrop_dl/crawlers/mixdrop.py
+++ cyberdrop_dl/crawlers/mixdrop.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import asyncio
 from typing import TYPE_CHECKING, ClassVar
 
 from cyberdrop_dl.crawlers.crawler import Crawler, SupportedDomains, SupportedPaths
 from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
-from cyberdrop_dl.utils import css, jsunpacker
+from cyberdrop_dl.utils import css, js_unpacker
 from cyberdrop_dl.utils.utilities import error_handling_wrapper
 
 if TYPE_CHECKING:
@@ -34,27 +35,36 @@ class MixDropCrawler(Crawler):
 
     @error_handling_wrapper
     async def file(self, scrape_item: ScrapeItem, file_id: str) -> None:
-        video_url = MixDropCrawler.PRIMARY_URL / "f" / file_id
 
-        if await self.check_complete(video_url, video_url):
+        embed_url = self.PRIMARY_URL / "e" / file_id
+
+        if await self.check_complete(embed_url, embed_url):
             return
 
-        scrape_item.url = video_url
-        soup = await self.request_soup(video_url)
-        title = css.select_text(soup, "div.tbl-c.title b")
-        link = await self._request_download(file_id)
+        scrape_item.url = embed_url
+        title, link = await self._request_file_info(file_id)
         filename, ext = self.get_filename_and_ext(title)
-        await self.handle_file(video_url, scrape_item, title, ext, custom_filename=filename, debrid_link=link)
+        await self.handle_file(
+            scrape_item.url,
+            scrape_item,
+            title,
+            ext,
+            custom_filename=filename,
+            debrid_link=link,
+        )
 
-    async def _request_download(self, file_id: str) -> AbsoluteHttpURL:
-        embed_url = MixDropCrawler.PRIMARY_URL / "e" / file_id
-        html = await self.request_text(embed_url)
-        info = dict(_extract_info(html))
-        return self.parse_url(info["wurl"])
+    async def _request_file_info(self, file_id: str) -> tuple[str, AbsoluteHttpURL]:
+        video_url = self.PRIMARY_URL / "f" / file_id
+        embed_url = self.PRIMARY_URL / "e" / file_id
+
+        soup, embed_html = await asyncio.gather(self.request_soup(video_url), self.request_text(embed_url))
+        title = css.select_text(soup, "div.tbl-c.title b")
+        md_props = dict(_extract_properties(embed_html))
+        return title, self.parse_url(md_props["wurl"])
 
 
-def _extract_info(html: str) -> Generator[tuple[str, str]]:
-    content = jsunpacker.unpack(html)
+def _extract_properties(html: str) -> Generator[tuple[str, str]]:
+    content = js_unpacker.unpack(html)
     for line in content.split(";MDCore."):
         name, _, value = line.partition("=")
         yield name.removeprefix("MDCore."), value.strip('"').strip()
