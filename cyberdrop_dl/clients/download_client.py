d6d1b11e29598a1276f7b75220e1eaf03d16e850
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-13 10:35:13 -0500

    refactor: update crawlers

--- cyberdrop_dl/clients/download_client.py
+++ cyberdrop_dl/clients/download_client.py
@@ -14,7 +14,6 @@ import aiofiles
 from cyberdrop_dl import aio, constants, storage
 from cyberdrop_dl.clients.response import AbstractResponse
 from cyberdrop_dl.constants import FileExt
-from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL
 from cyberdrop_dl.exceptions import DDOSGuardError, DownloadError, InvalidContentTypeError, SlowDownloadError
 from cyberdrop_dl.utils import dates
 
@@ -25,7 +24,7 @@ if TYPE_CHECKING:
 
     import aiohttp
 
-    from cyberdrop_dl.data_structures.url_objects import MediaItem
+    from cyberdrop_dl.data_structures.url_objects import AbsoluteHttpURL, MediaItem
     from cyberdrop_dl.managers.client_manager import ClientManager
     from cyberdrop_dl.managers.manager import Manager
 
@@ -35,9 +34,6 @@ logger = logging.getLogger(__name__)
 
 _CONTENT_TYPES_OVERRIDES: dict[str, str] = {"text/vnd.trolltech.linguist": "video/MP2T"}
 _SLOW_DOWNLOAD_PERIOD: int = 10  # seconds
-_CHROME_ANDROID_USER_AGENT: str = (
-    "Mozilla/5.0 (Linux; Android 16) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.7204.180 Mobile Safari/537.36"
-)
 _FREE_SPACE_CHECK_PERIOD: int = 5  # Check every 5 chunks
 _NULL_CONTEXT: contextlib.nullcontext[None] = contextlib.nullcontext()
 _USE_IMPERSONATION: set[str] = {"vsco", "celebforum"}
@@ -64,33 +60,6 @@ class DownloadClient:
     async def _track_errors(self, domain: str):
         with self.client_manager.request_context(domain):
             yield
-
-    def _get_download_headers(self, domain: str, referer: AbsoluteHttpURL) -> dict[str, str]:
-        download_headers = {
-            "User-Agent": self.manager.config.global_settings.general.user_agent,
-            "Referer": str(referer),
-        }
-        auth_data = self.manager.config.auth
-        if domain == "pixeldrain" and auth_data.pixeldrain.api_key:
-            download_headers["Authorization"] = self.manager.client_manager.basic_auth(
-                "Cyberdrop-DL", auth_data.pixeldrain.api_key
-            )
-        elif domain == "gofile":
-            gofile_cookies = self.client_manager.cookies.filter_cookies(AbsoluteHttpURL("https://gofile.io"))
-            api_key = gofile_cookies.get("accountToken", "")
-            if api_key:
-                download_headers["Authorization"] = f"Bearer {api_key.value}"  # type: ignore
-        elif domain == "odnoklassniki":
-            # TODO: Add "headers" attribute to MediaItem to use custom headers for downloads
-            download_headers |= {
-                "Accept-Language": "en-gb, en;q=0.8",
-                "User-Agent": _CHROME_ANDROID_USER_AGENT,
-                "Referer": "https://m.ok.ru/",
-                "Origin": "https://m.ok.ru",
-            }
-        elif domain == "megacloud":
-            download_headers["Referer"] = "https://megacloud.blog/"
-        return download_headers
 
     async def _download(self, domain: str, media_item: MediaItem) -> bool:
         """Downloads a file."""
