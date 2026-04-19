6e23b6fb39f3657cbded9cb2f1306a0ba4dfe7ea
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-18 20:10:29 -0500

    refactor: add script to find subclasses

---
+++ scripts/tools/find_subclasses.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import importlib.util
+import sys
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import rich
+from rich.table import Table
+
+from cyberdrop_dl.progress import hyperlink
+
+if TYPE_CHECKING:
+    from cyberdrop_dl.crawlers.crawler import Crawler
+
+
+def find_subclasses_of(domain: str):
+    from cyberdrop_dl.crawlers.crawler import Registry
+
+    Registry.import_all()
+
+    if domain.endswith("Crawler"):
+        crawlers = {c.__name__: c for c in Registry.abc | Registry.generic | Registry.concrete}
+    else:
+        crawlers = {c.DOMAIN: c for c in Registry.abc | Registry.generic | Registry.concrete}
+    target = crawlers[domain]
+    return dict(sorted((c.__name__, c) for c in crawlers.values() if issubclass(c, target) and c is not target))
+
+
+def module_path(cls: type):
+    spec = importlib.util.find_spec(cls.__module__)
+    assert spec and spec.origin
+    return hyperlink(Path(spec.origin))
+
+
+def make_table(subclasses: dict[str, type[Crawler]]):
+
+    table = Table(
+        show_lines=True,
+        highlight=True,
+    )
+    for column in ("name", "URL", "path"):
+        table.add_column(column, no_wrap=True)
+
+    for name, crawler in subclasses.items():
+        table.add_row(name, str(getattr(crawler, "PRIMARY_URL", None)), module_path(crawler))
+
+    return table
+
+
+if __name__ == "__main__":
+    domain = sys.argv[1]
+    subclasses = find_subclasses_of(domain)
+    table = make_table(subclasses)
+    rich.print(table)
